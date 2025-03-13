fb63d1fd8ade022515eddce6e49445c33528ea7e
sbarranco <sbarrancovico@gmail.com>
2025-03-13 17:32:20 +0100

    feat(assets): update asset paths and add new images; configure environment settings

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,16 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { enableProdMode } from '@angular/core';
+import { environment } from './environments/environment';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+if (environment.production) {
+  enableProdMode();
+} else {
+  const { worker } = require('./mocks/browser');
+  worker.start();
+}
+
+bootstrapApplication(AppComponent, appConfig).catch((err) =>
+  console.error(err)
+);
