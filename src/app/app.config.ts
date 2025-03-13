dcbd9166f6d90923eca6c0748a95a2bdc0056be9
sbarranco <sbarrancovico@gmail.com>
2025-03-13 23:04:33 +0100

    feat(app): configure NgRx store and effects with devtools for state management

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,24 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideEffects } from '@ngrx/effects';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { routes } from './app.routes';
+import { ItemEffects } from './state/store/effects/item.effects';
+import { itemReducerReducerFunction } from './state/store/reducers/item.reducer';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
+    provideStore(itemReducerReducerFunction),
+    provideEffects([ItemEffects]),
+    provideStoreDevtools({
+      maxAge: 25,
+      logOnly: environment.production,
+    }),
   ],
 };
