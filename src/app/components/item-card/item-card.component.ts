5f66be6ea76f04a828b45f0eaced765ff2f355a7
sbarranco <sbarrancovico@gmail.com>
2025-03-14 18:56:18 +0100

    feat(item-card): add item card component with details and favorite functionality

---
+++ src/app/components/item-card/item-card.component.ts
@@ -0,0 +1,14 @@
+import { Component, input, Input, output } from '@angular/core';
+import { Item } from '../../state/models/item.model';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-item-card',
+  templateUrl: './item-card.component.html',
+  styleUrls: ['./item-card.component.scss'],
+  imports: [CommonModule],
+})
+export class ItemCardComponent {
+  item = input.required<Item>();
+  triggerFavorite = output<Item>();
+}
--- src/app/components/item-list/item-list.component.ts
+++ src/app/components/item-list/item-list.component.ts
@@ -1,46 +1,78 @@
-import { Component, HostBinding, inject, OnInit } from '@angular/core';
-import { ItemService } from '../../services/items.service';
-import { Item } from '../../models/item.model';
 import { CommonModule } from '@angular/common';
+import { Component, HostBinding, inject, OnInit, signal } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { AppFacade } from '../../state/facades/app.facade';
+import { Item, Pagination } from '../../state/models/item.model';
+import { ItemCardComponent } from '../item-card/item-card.component';
 
 @Component({
   selector: 'app-item-list',
   templateUrl: './item-list.component.html',
   styleUrls: ['./item-list.component.scss'],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule, ItemCardComponent],
 })
 export class ItemListComponent implements OnInit {
   @HostBinding('class') class = 'app-item-list';
-  private itemService = inject(ItemService);
+  private AppFacade = inject(AppFacade);
 
-  items: Item[] = [];
+  loading$ = this.AppFacade.loading$;
+  items$ = this.AppFacade.items$;
 
-  limit = 5;
-  offset = 0;
-  isLoading = false;
-  hasMoreItems = true;
+  showMoreFilters = signal(false);
+  hasMoreItems = signal(false);
+  pagination = signal<Pagination>({
+    limit: 5,
+    offset: 0,
+  });
+
+  searchForm: FormGroup = new FormGroup({
+    searchPrompt: new FormControl('', [Validators.maxLength(120)]),
+  });
+
+  filterForm: FormGroup = new FormGroup({
+    title: new FormControl('', [Validators.maxLength(120)]),
+    price: new FormControl('', [Validators.pattern(/^\d+$/)]),
+    email: new FormControl('', [Validators.email]),
+  });
 
   ngOnInit(): void {
     this.loadItems();
   }
 
   loadItems(): void {
-    this.isLoading = true;
-    this.itemService.getItems(this.limit, this.offset).subscribe((data) => {
-      this.items = [...this.items, ...data.items];
-      this.offset += this.limit;
-      this.hasMoreItems = data.items.length === this.limit;
-      this.isLoading = false;
-    });
+    this.AppFacade.loadItems(this.pagination());
   }
 
   onScroll(): void {
-    if (!this.isLoading) {
-      this.loadItems();
+    this.pagination.set({
+      limit: this.pagination().limit,
+      offset: this.pagination().offset + this.pagination().limit,
+    });
+    this.loadItems();
+  }
+
+  onClickSearchItems(): void {
+    console.log('Searching for:', this.searchForm.value);
+    console.log('Searching for:', this.filterForm.value);
+    this.searchForm.get('searchPrompt')!.markAsTouched();
+    if (this.searchForm.invalid) {
+      return;
+    } else {
+      this.AppFacade.searchItems(this.searchForm.value);
     }
   }
 
-  selectItem(item: Item): void {
-    console.log('Selected item:', item);
+  onClickShowMoreFilters(): void {
+    this.showMoreFilters.set(!this.showMoreFilters());
+  }
+
+  onTriggerFavorite(item: Item): void {
+    console.log('Favorite item:', item);
   }
 }
