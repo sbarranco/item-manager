2c52aeb6566567642fced4a7d505ffe69ff23830
sbarranco <sbarrancovico@gmail.com>
2025-03-14 18:56:04 +0100

    feat(app): integrate favorites modal and enhance app layout with navigation and footer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,29 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { ItemListComponent } from './components/item-list/item-list.component';
+import { FavoritesModalComponent } from './components/favorites-modal/favorites-modal.component';
+import { AppFacade } from './state/facades/app.facade';
+import { Item } from './state/models/item.model';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, ItemListComponent],
+  imports: [CommonModule, ItemListComponent, FavoritesModalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'item-manager';
+  private AppFacade = inject(AppFacade);
+
+  favorites$ = this.AppFacade.favoriteItems$;
+
+  isFavoritesModalOpen = signal(false);
+
+  openFavoritesModal(): void {
+    this.isFavoritesModalOpen.set(!this.isFavoritesModalOpen());
+  }
+
+  onDeleteFavorites(item: Item): void {
+    this.AppFacade.deleteFavoriteItem(item);
+  }
 }
